4a3c2b5d2a69f900974d264299b34d935255502a
zhengyongzhe <zhengyongzhezhe@gmail.com>
2017-10-16 22:07:03 +0800

    add time_cost

--- 001. Two Sum/TwoSum.py
+++ 001. Two Sum/TwoSum.py
@@ -1,11 +1,15 @@
 # coding=utf-8
+import sys
+sys.path.append('../')
 import numpy as np
+from lib import *
 
 
 class TwoSum(object):
     """docstring for Two Sum"""
     def __init__(self):
         super(TwoSum, self).__init__()
+        self.twosum()
 
     def twosum(self, random_list=None, target=None):
         """
@@ -13,25 +17,36 @@ class TwoSum(object):
         :type target: int
         :rtype: List[int]
         """
-        if random_list is None:
-            random_list = np.random.randint(2017, size=20)
-        if target is None:
-            target = random_list[0] + random_list[-1]
+        self.random_list = random_list
+        self.target = target
 
-        # # solution1 : Brute Force
-        # for index1, number1 in enumerate(random_list):
-        #     for index2, number2 in enumerate(random_list):
-        #         if (number1 + number2 == target) and index1 != index2:
-        #             return index1, index2
+        if self.random_list is None:
+            self.random_list = np.random.randint(2017, size=2000)
+        if self.target is None:
+            self.target = self.random_list[0] + self.random_list[-1]
 
+    @time_cost
+    def solution1(self):
+        # solution1 : Brute Force
+        for index1, number1 in enumerate(self.random_list):
+            for index2, number2 in enumerate(self.random_list):
+                if (number1 + number2 == self.target) and index1 != index2:
+                    return index1, index2
+
+    @time_cost
+    def solution2(self):
         # solution2 : Hash map
-        random_list = list(random_list)
-        for index1, number1 in enumerate(random_list):
-            number2 = target - number1
-            if number2 in random_list:
-                index2 = random_list.index(number2)
+        self.random_list = list(self.random_list)
+        for number1 in self.random_list:
+            number2 = self.target - number1
+            if number2 in self.random_list:
+                # index1 = np.where(self.random_list == number1)
+                # index2 = np.where(self.random_list == number2)
+                index1 = self.random_list.index(number1)
+                index2 = self.random_list.index(number2)
                 return index1, index2
 
 
 instan = TwoSum()
-print instan.twosum()
+print instan.solution1()
+print instan.solution2()
