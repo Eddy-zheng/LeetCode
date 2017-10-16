584e99aef3cf3c864724ae3115153218a4d28489
zhengyongzhe <zhengyongzhezhe@gmail.com>
2017-10-16 21:05:35 +0800

    001 solution 2 : Hash map

--- 001. Two Sum/TwoSum.py
+++ 001. Two Sum/TwoSum.py
@@ -13,16 +13,24 @@ class TwoSum(object):
         :type target: int
         :rtype: List[int]
         """
-        # solution1 : Brute Force
         if random_list is None:
             random_list = np.random.randint(2017, size=20)
         if target is None:
             target = random_list[0] + random_list[-1]
 
+        # # solution1 : Brute Force
+        # for index1, number1 in enumerate(random_list):
+        #     for index2, number2 in enumerate(random_list):
+        #         if (number1 + number2 == target) and index1 != index2:
+        #             return index1, index2
+
+        # solution2 : Hash map
+        random_list = list(random_list)
         for index1, number1 in enumerate(random_list):
-            for index2, number2 in enumerate(random_list):
-                if (number1 + number2 == target) and index1 != index2:
-                    return index1, index2
+            number2 = target - number1
+            if number2 in random_list:
+                index2 = random_list.index(number2)
+                return index1, index2
 
 
 instan = TwoSum()
